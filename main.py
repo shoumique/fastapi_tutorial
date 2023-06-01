36c2ab24134ae0daf6c7693d5e10bb9e0f276290
Md. Shoumique Hasan <shoumique.hasan@bjitgroup.com>
2023-06-01 12:31:57 +0600

    Basic sqlalchemy CRUD Operations

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+    # rating: Optional[int] = None
 
 while True:
     try:
@@ -59,28 +59,43 @@ def test_posts(db: Session = Depends(get_db)):
 
 
 @app.get("/posts")
-def get_posts():
+def get_posts(db: Session = Depends(get_db)):
 
-    cursor.execute("""
-        SELECT *
-        FROM posts
-    """)
+    # cursor.execute("""
+    #     SELECT *
+    #     FROM posts
+    # """)
+    # posts = cursor.fetchall()
 
-    posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
 
     return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
+def create_posts(post: Post, db: Session = Depends(get_db)):
 
-    cursor.execute("""
-        INSERT INTO posts (title, content, is_published) VALUES (%s, %s, %s) RETURNING * 
-    """, (post.title, post.content, post.published))
+    # Raw SQL approach starts
+    # cursor.execute("""
+    #     INSERT INTO posts (title, content, is_published) VALUES (%s, %s, %s) RETURNING * 
+    # """, (post.title, post.content, post.published))
+    
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    # Raw SQL approach ends
 
-    new_post = cursor.fetchone()
+    # ORM approach
+    # first map the fields
+    # new_post = models.Post(title=post.title, content=post.content, published=post.published)
 
-    conn.commit()
+    # But this is not a good approach because, if we have 50 fields we need to do this for 50 fields)
+    # So better approach is to use pydantic model to convert to dict and then unpacking the dict
+    new_post = models.Post(**post.dict())
+
+    db.add(new_post)
+    db.commit()
+
+    db.refresh(new_post)
 
     return {"data": new_post}
 
@@ -90,15 +105,19 @@ def create_posts(post: Post):
 #     return {"detail": "this is the latest post"}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, db: Session = Depends(get_db)):
     
-    cursor.execute("""
-        SELECT *
-        FROM posts
-        WHERE id = %s
-    """, (str(id),))
+    # Raw SQL approach
+    # cursor.execute("""
+    #     SELECT *
+    #     FROM posts
+    #     WHERE id = %s
+    # """, (str(id),))
 
-    post = cursor.fetchone()
+    # post = cursor.fetchone()
+
+    # SQLALCHEMY or ORM approach
+    post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
         # response.status_code = status.HTTP_404_NOT_FOUND
@@ -110,40 +129,61 @@ def get_post(id: int):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""
-        DELETE FROM posts
-        WHERE id=%s RETURNING *
-    """, (str(id),))
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""
+    #     DELETE FROM posts
+    #     WHERE id=%s RETURNING *
+    # """, (str(id),))
 
-    deleted_post = cursor.fetchone()
+    # deleted_post = cursor.fetchone()
 
-    conn.commit()
+    # conn.commit()
 
-    if deleted_post == None:
+    # This is only just query
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    # for executing the query we need to add all() or first()
+
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id: {id} does not exist")
+    
+    post.delete(synchronize_session=False)
+    db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
 
-    cursor.execute("""
-        UPDATE posts
-        SET title = %s, content = %s, is_published = %s 
-        WHERE id = %s 
-        RETURNING *
-    """, (post.title, post.content, post.published, str(id)))
+    # cursor.execute("""
+    #     UPDATE posts
+    #     SET title = %s, content = %s, is_published = %s 
+    #     WHERE id = %s 
+    #     RETURNING *
+    # """, (post.title, post.content, post.published, str(id)))
 
-    updated_post = cursor.fetchone()
+    # updated_post = cursor.fetchone()
 
-    conn.commit()
+    # conn.commit()
 
-    if updated_post == None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id: {id} does not exist")
     
-    return {"data": updated_post}
+    # post_query.update({
+    #     'title': 'updated title',
+    #     'content': 'this is updated content'
+    # }, synchronize_session=False)
+
+    post_query.update(updated_post.dict(), synchronize_session=False)
+
+    db.commit()
+    
+    return {"data": post_query.first()}
 
