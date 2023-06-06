b4c21fddb5541e6f4f93430a1073bb424da1c78e
Md. Shoumique Hasan <shoumique.hasan@bjitgroup.com>
2023-06-06 16:21:08 +0600

    Implemented User Login: JWT Token - Using OAuth2PasswordRequestForm

--- routers/auth.py
+++ routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 import database, schemas, models, utils, oauth2
@@ -7,8 +8,17 @@ router = APIRouter(tags=['Authentication'],)
 
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    # So OAuth2PasswordRequestForm stores the credentials in username
+    # So there will be nothing like user_credentials.email now
+    # the email will be saved in username
+    # So there will be two fields in user_credentials dictionary
+    # user_credentials = {
+    #     "username": "the email will be here",
+    #     "password": "password will be here"
+    # }
+    
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
