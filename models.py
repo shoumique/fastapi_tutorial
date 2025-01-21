5f796c32b4771cf23f6c8e1ca3314e0b0492d35a
Shoumique Hasan <shoumique@gmail.com>
2025-01-21 12:43:08 +0600

    Update models.py added a ai_generated boolean field

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class User(Base):
 
 
 class Post(Base):
-    __tablename__= "posts"
+    __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
@@ -26,7 +26,7 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-
+    is_ai_generated = Column(Boolean, server_default='FALSE', nullable=False)
 
     owner = relationship("User")
 
