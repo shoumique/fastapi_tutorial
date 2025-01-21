dea0c37b5f0abbdb94d65913a92d69b0507ff074
Shoumique Hasan <shoumique@gmail.com>
2025-01-21 12:46:34 +0600

    Update main.py added post_ai router

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routers import post, user, auth
+from routers import post, user, auth, post_ai
 import models
 from database import engine
 
 app = FastAPI()
 
 app.include_router(post.router)
+app.include_router(post_ai.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 
